fbb87df950651a6bcad13e082c69655878b6565f
Michel <github@chromadigital.group>
2019-05-28 15:18:05 +0200

    added converters for the different enum types

---
+++ source/AsyncKeyState/Converter/KeyCodeConverter.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Windows.Forms;
+using System.Windows.Input;
+
+namespace AsyncKeyState.Converter
+{
+    public static class KeyCodeConverter
+    {
+        public static int ToInt(Key key)
+        {
+            if (ValidationHelper.IsKeyOutOfRange(key)) throw ThrowHelper.InvalidEnumArgumentException<Key>(nameof(key), key);
+
+            return (int)key;
+        }
+
+        public static int ToInt(Keys key)
+        {
+            if (ValidationHelper.IsKeyOutOfRange(key)) throw ThrowHelper.InvalidEnumArgumentException<Keys>(nameof(key), key);
+
+            return (int)key;
+        }
+
+        public static string ToString(Key key)
+        {
+            if (ValidationHelper.IsKeyOutOfRange(key)) throw ThrowHelper.InvalidEnumArgumentException<Key>(nameof(key), key);
+
+            return key.ToString();
+        }
+
+        public static string ToString(Keys key)
+        {
+            if (ValidationHelper.IsKeyOutOfRange(key)) throw ThrowHelper.InvalidEnumArgumentException<Keys>(nameof(key), key);
+
+            return key.ToString();
+        }
+
+        public static Key WPFKeyFromString(string value)
+        {
+            if (value == null) throw ThrowHelper.ArgumentNullException(nameof(value));
+            if (value.Length == 0) throw ThrowHelper.ArgumentOutOfRangeException(nameof(value));
+
+            if (Enum.TryParse<Key>(value, true, out var result))
+            {
+                return result;
+            }
+            else
+            {
+                throw ThrowHelper.EnumParseException<Key>(value);
+            }
+        }
+
+        public static Keys FormsKeyFromString(string value)
+        {
+            if (value == null) throw ThrowHelper.ArgumentNullException(nameof(value));
+            if (value.Length == 0) throw ThrowHelper.ArgumentOutOfRangeException(nameof(value));
+
+            if (Enum.TryParse<Keys>(value, true, out var result))
+            {
+                return result;
+            }
+            else
+            {
+                throw ThrowHelper.EnumParseException<Keys>(value);
+            }
+        }
+
+        public static Key FormsKeyToWPFKey(Keys key)
+        {
+            if (ValidationHelper.IsKeyOutOfRange(key)) throw ThrowHelper.InvalidEnumArgumentException<Keys>(nameof(key), key);
+
+            return KeyInterop.KeyFromVirtualKey((int)key);
+        }
+
+        public static Keys WPFKeyToFormsKey(Key key)
+        {
+            if (ValidationHelper.IsKeyOutOfRange(key)) throw ThrowHelper.InvalidEnumArgumentException<Key>(nameof(key), key);
+
+            return (Keys)KeyInterop.VirtualKeyFromKey(key);
+        }
+    }
+}
---
+++ source/AsyncKeyState/Converter/KeyStatesConverter.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Windows.Input;
+
+namespace AsyncKeyState.Converter
+{
+    public static class KeyStatesConverter
+    {
+        public static int ToInt(KeyStates state)
+        {
+            if (ValidationHelper.IsKeyStatesOutOfRange(state)) throw ThrowHelper.InvalidEnumArgumentException<KeyStates>(nameof(state), state);
+
+            return (int)state;
+        }
+
+        public static string ToString(KeyStates state)
+        {
+            if (ValidationHelper.IsKeyStatesOutOfRange(state)) throw ThrowHelper.InvalidEnumArgumentException<KeyStates>(nameof(state), state);
+
+            return state.ToString();
+        }
+
+        public static KeyStates FromString(string value)
+        {
+            if (value == null) throw ThrowHelper.ArgumentNullException(nameof(value));
+            if (value.Length == 0) throw ThrowHelper.ArgumentOutOfRangeException(nameof(value));
+
+            if (Enum.TryParse<KeyStates>(value, true, out var result))
+            {
+                return result;
+            }
+            else
+            {
+                throw ThrowHelper.EnumParseException<KeyStates>(value);
+            }
+        }
+    }
+}
