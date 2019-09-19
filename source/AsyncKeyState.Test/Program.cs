5242d6c93fa09a9eb9d3a1fcac62c02701cbcd4b
michel-pi <github@michel-pi.de>
2019-09-20 01:05:10 +0200

    added test project

---
+++ source/AsyncKeyState.Test/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Windows.Forms;
+using AsyncKeyState;
+
+namespace AsyncKeyState.Test
+{
+	internal static class Program
+	{
+		private static void Main()
+		{
+			InputGenerator.Keyboard.SendInput(Keys.Space);
+		}
+	}
+}
