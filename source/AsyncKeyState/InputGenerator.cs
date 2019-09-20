a381118e1c7a76d984b892957847ecf81f4b6361
michel-pi <github@michel-pi.de>
2019-09-20 07:10:14 +0200

    added missing docs and removed unused methods

--- source/AsyncKeyState/InputGenerator.cs
+++ source/AsyncKeyState/InputGenerator.cs
@@ -9,12 +9,12 @@ using AsyncKeyState.Internal;
 namespace AsyncKeyState
 {
 	/// <summary>
-	/// Synthesizes custom Mouse, Keyboard and Window input.
+	/// Synthesizes custom mouse, keyboard and window input.
 	/// </summary>
 	public static class InputGenerator
 	{
 		/// <summary>
-		/// Synthesizes Keyboard events.
+		/// Synthesizes keyboard events.
 		/// </summary>
 		public static class Keyboard
 		{
@@ -22,7 +22,7 @@ namespace AsyncKeyState
 			/// Presses a key on the Keyboard.
 			/// </summary>
 			/// <param name="key">The key to press.</param>
-			/// <param name="useKeyboardEvent">Determines whether to use the keyboard_event api or not.</param>
+			/// <param name="useKeyboardEvent">Determines whether to use the keyboard_event API or not.</param>
 			/// <returns>Returns true on success.</returns>
 			public static bool Press(Keys key, bool useKeyboardEvent = false)
 				=> Press(key, KeyStates.Toggled, useKeyboardEvent);
@@ -32,7 +32,7 @@ namespace AsyncKeyState
 			/// </summary>
 			/// <param name="key">The key.</param>
 			/// <param name="state">The new state of the given key.</param>
-			/// <param name="useKeyboardEvent">Determines whether to use the keyboard_event api or not.</param>
+			/// <param name="useKeyboardEvent">Determines whether to use the keyboard_event API or not.</param>
 			/// <returns>Returns true on success.</returns>
 			public static bool Press(Keys key, KeyStates state, bool useKeyboardEvent = false)
 			{
@@ -44,15 +44,39 @@ namespace AsyncKeyState
 					: KeyboardInputHelper.SendKeyboardInput(key, state);
 			}
 
+			/// <summary>
+			/// Presses a key on the Keyboard and immediately returns.
+			/// </summary>
+			/// <param name="key">The key to press.</param>
+			/// <param name="useKeyboardEvent">Determines whether to use the keyboard_event API or not.</param>
+			/// <returns>Returns a Task<bool></bool>.</returns>
 			public static Task<bool> PressAsync(Keys key, bool useKeyboardEvent = false)
 				=> Task.Factory.StartNew(() => Press(key, useKeyboardEvent));
 
+			/// <summary>
+			/// Changes the state of a key on the Keyboard and immediately returns.
+			/// </summary>
+			/// <param name="key">The key.</param>
+			/// <param name="state">The new state of the given key.</param>
+			/// <param name="useKeyboardEvent">Determines whether to use the keyboard_event API or not.</param>
+			/// <returns>Returns a Task<bool></bool>.</returns>
 			public static Task<bool> PressAsync(Keys key, KeyStates state, bool useKeyboardEvent = false)
 				=> Task.Factory.StartNew(() => Press(key, state, useKeyboardEvent));
 		}
 
+		/// <summary>
+		/// Synthesizes mouse events.
+		/// </summary>
 		public static class Mouse
 		{
+			/// <summary>
+			/// Moves the mouse to a specific coordinate on the screen or relative by an amount of pixels.
+			/// </summary>
+			/// <param name="x">The absolute x-coordinate of the mouse or a relative amount of pixels.</param>
+			/// <param name="y">The absolute y-coordinate of the mouse or a relative amount of pixels.</param>
+			/// <param name="relative">Determines whether the coordinates are absolute.</param>
+			/// <param name="useMouseEvent">Determines whether to use the mouse_event API or not.</param>
+			/// <returns>Returns true on success.</returns>
 			public static bool Move(int x, int y, bool relative = false, bool useMouseEvent = false)
 			{
 				return useMouseEvent
@@ -60,12 +84,33 @@ namespace AsyncKeyState
 					: MouseInputHelper.SendMouseMoveInput(x, y, relative);
 			}
 
+			/// <summary>
+			/// Moves the mouse to a specific coordinate on the screen or relative by an amount of pixels and returns immediately.
+			/// </summary>
+			/// <param name="x">The absolute x-coordinate of the mouse or a relative amount of pixels.</param>
+			/// <param name="y">The absolute y-coordinate of the mouse or a relative amount of pixels.</param>
+			/// <param name="relative">Determines whether the coordinates are absolute.</param>
+			/// <param name="useMouseEvent">Determines whether to use the mouse_event API or not.</param>
+			/// <returns>Returns a Task<bool></bool>.</returns>
 			public static Task<bool> MoveAsync(int x, int y, bool relative = false, bool useMouseEvent = false)
 				=> Task.Factory.StartNew(() => Move(x, y, relative, useMouseEvent));
 
+			/// <summary>
+			/// Presses a button on the mouse.
+			/// </summary>
+			/// <param name="key">The mouse button to press.</param>
+			/// <param name="useMouseEvent">Determines whether to use the mouse_event API or not.</param>
+			/// <returns>Returns true on success.</returns>
 			public static bool Press(Keys key, bool useMouseEvent = false)
 										=> Press(key, KeyStates.Toggled, useMouseEvent);
 
+			/// <summary>
+			/// Presses a button on the mouse.
+			/// </summary>
+			/// <param name="key">The mouse button to press.</param>
+			/// <param name="state">The new state of the mouse button.</param>
+			/// <param name="useMouseEvent">Determines whether to use the mouse_event API or not.</param>
+			/// <returns>Returns true on success.</returns>
 			public static bool Press(Keys key, KeyStates state, bool useMouseEvent = false)
 			{
 				if (ValidationHelper.IsKeyOutOfRange(key)) ThrowHelper.ArgumentOutOfRangeException(nameof(key));
@@ -76,15 +121,39 @@ namespace AsyncKeyState
 					: MouseInputHelper.SendMouseInput(key, state);
 			}
 
+			/// <summary>
+			/// Presses a button on the mouse and returns immediately.
+			/// </summary>
+			/// <param name="key">The mouse button to press.</param>
+			/// <param name="useMouseEvent">Determines whether to use the mouse_event API or not.</param>
+			/// <returns>Returns a Task<bool></bool>.</returns>
 			public static Task<bool> PressAsync(Keys key, bool useMouseEvent = false)
 				=> Task.Factory.StartNew(() => Press(key, useMouseEvent));
 
+			/// <summary>
+			/// Presses a button on the mouse and returns immediately.
+			/// </summary>
+			/// <param name="key">The mouse button to press.</param>
+			/// <param name="state">The new state of the mouse button.</param>
+			/// <param name="useMouseEvent">Determines whether to use the mouse_event API or not.</param>
+			/// <returns>Returns a Task<bool></bool>.</returns>
 			public static Task<bool> PressAsync(Keys key, KeyStates state, bool useMouseEvent = false)
 				=> Task.Factory.StartNew(() => Press(key, state, useMouseEvent));
 		}
 
+		/// <summary>
+		/// Synthesizes mouse and keyboard events for a window or control.
+		/// </summary>
 		public static class Window
 		{
+			/// <summary>
+			/// Moves the mouse relative to the window. PostMessage alows async execution and returns immediately.
+			/// </summary>
+			/// <param name="windowHandle">The handle of a Window or Control.</param>
+			/// <param name="x">The new x-coordinate of the mouse.</param>
+			/// <param name="y">The new y-coordinate of the mouse.</param>
+			/// <param name="usePostMessage">Determines whether to use the PostMessage API or not.</param>
+			/// <returns>Returns true on success.</returns>
 			public static bool MoveMouse(IntPtr windowHandle, int x, int y, bool usePostMessage = false)
 			{
 				return usePostMessage
@@ -92,24 +161,30 @@ namespace AsyncKeyState
 					: WindowInputHelper.SendMouseMoveMessage(windowHandle, x, y);
 			}
 
-			public static Task<bool> MoveMouseAsync(IntPtr windowHandle, int x, int y, bool usePostMessage = false)
-				=> Task.Factory.StartNew(() => MoveMouse(windowHandle, x, y, usePostMessage));
-
+			/// <summary>
+			/// Presses a key (mouse or keyboard) inside a window or control. PostMessage alows async execution and returns immediately.
+			/// </summary>
+			/// <param name="windowHandle">The handle of a Window or Control.</param>
+			/// <param name="key">The key to press.</param>
+			/// <param name="usePostMessage">Determines whether to use the PostMessage API or not.</param>
+			/// <returns>Returns true on success.</returns>
 			public static bool PressKey(IntPtr windowHandle, Keys key, bool usePostMessage = false)
 										=> PressKey(windowHandle, key, KeyStates.Toggled, usePostMessage);
 
+			/// <summary>
+			/// Presses a key (mouse or keyboard) inside a window or control. PostMessage alows async execution and returns immediately.
+			/// </summary>
+			/// <param name="windowHandle">The handle of a Window or Control.</param>
+			/// <param name="key">The key.</param>
+			/// <param name="state">The state of the given key.</param>
+			/// <param name="usePostMessage">Determines whether to use the PostMessage API or not.</param>
+			/// <returns>Returns true on success.</returns>
 			public static bool PressKey(IntPtr windowHandle, Keys key, KeyStates state, bool usePostMessage = false)
 			{
 				return usePostMessage
 					? WindowInputHelper.PostMessage(windowHandle, key, state)
 					: WindowInputHelper.SendMessage(windowHandle, key, state);
 			}
-
-			public static Task<bool> PressKeyAsync(IntPtr windowHandle, Keys key, bool usePostMessage = false)
-				=> Task.Factory.StartNew(() => PressKey(windowHandle, key, usePostMessage));
-
-			public static Task<bool> PressKeyAsync(IntPtr windowHandle, Keys key, KeyStates state, bool usePostMessage = false)
-				=> Task.Factory.StartNew(() => PressKey(windowHandle, key, state, usePostMessage));
 		}
 	}
 }
